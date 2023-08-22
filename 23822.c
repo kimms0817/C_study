5491627c57f7bfda0be5dcddf04f567c091d5eca
kimms0817 <141706952+kimms0817@users.noreply.github.com>
2023-08-22 16:06:32 +0900

    Update 23822.c

--- 23822.c
+++ 23822.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+
+int main(void)
+{
+  int x,y;
+  char code;
+  scanf("%c",&code);
+  if(code == 'X'){
+    x++;
+  }
+  else if(code == 'Y'){
+    y++;
+  }
+  else{
+    x=y=0;
+  }
+  
+  return 0;
+ }
