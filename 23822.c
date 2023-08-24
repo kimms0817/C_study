5b8100efacffc137e0c241b8aa45fcacc4bfa68c
kimms0817 <141706952+kimms0817@users.noreply.github.com>
2023-08-24 15:01:02 +0900

    Update 23822.c

--- 23822.c
+++ 23822.c
@@ -18,27 +18,3 @@ int main(void)
   
   return 0;
  }
-
-switch 문 특징 
-switch(number)
-{
-case x:
-  printf("x와 일치합니다.\n");
-  break;
-  변수 사용 X
-case(x+2):
-  printf("수식과 일치합니다.\n");
-  break;
-수식 사용 X
-case 0.001:
-  printf("실수");
-  break;
-실수 사용 X
-case "0.01":
-  printf("문자열");
-  break;
-문자열 사용 X
-case 'a'
-  printf("문자\n");
-  break;
-문자 사용 O
